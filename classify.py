25b4a87a47729d4d8341f7cf7217d49b55a322e8
Flora Xue <xuezhenruo@berkeley.edu>
2018-03-17 07:23:57 -0700

    add test message

--- classify.py
+++ classify.py
@@ -173,7 +173,8 @@ def run_training(args):
     test_actor = ResNetTestActor.remote()
 
     step = 0
-    print(model.state_dict().keys())
+    for m in model.modules():
+        print(m.parameters().keys())
     weight_id = ray.put(model.parameters()['weight'])
     acc_id = test_actor.validate.remote(weight_id, step)
 
